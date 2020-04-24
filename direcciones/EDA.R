0dc91ecba5461f9d88680c16e7d673df063437e3
Jose Ayerdis <joseayerdis@gmail.com>
2020-04-24 16:38:15 -0600

    Preprocessing of unorthodox addresses with regex

--- direcciones/EDA.R
+++ direcciones/EDA.R
@@ -28,14 +28,16 @@ preprocess_original_file <- function(source_file = FILE_ADDRESS_1, target_file =
 
 address_filtered <- preprocess_original_file(FILE_ADDRESS_1, TARGET_ADDRESS_FILE) 
 
-
+addresses <- function(address_filtered) {
+  address_filtered %>% mutate(Address = paste(Address1, str_replace_na(Address2, ""))) %>% 
+    # Select only the newly created Address column and department
+    select(Department, Address) %>% 
+    # Getting all the words for each Address
+    unnest_tokens(word, Address)  
+}
 
 # Having the address1 and address2 together for unigram count
-address_filtered %>% mutate(Address = paste(Address1, str_replace_na(Address2, ""))) %>% 
-                     # Select only the newly created Address column and department
-                     select(Department, Address) %>% 
-                     # Getting all the words for each Address
-                     unnest_tokens(word, Address) %>% group_by(Department, word) %>% 
+addresses(address_filtered) %>% group_by(Department, word) %>% 
                      # counting frequency per word per department, sort med desc, and filter only those with more than 50 in frequency
                      summarise(n = n()) %>% top_n(10) %>% arrange(-n) %>% 
                      ungroup() %>% filter(n > 10) %>%
@@ -48,17 +50,30 @@ address_filtered %>% mutate(Address = paste(Address1, str_replace_na(Address2, "
                      ggtitle("Distribution of words frequency on address per department") +
                      xlab("Unigrams per department") + ylab("Frequency")
 
-# 
-# library(lubridate)
-#                     
-#                 
-# # S19 2018-2019
-# pre.s19 <- ymd("2018-09-28") # beg of preseason
-# # S20 2019-2020, included hiatus
-# pre.s20 <- ymd("2019-09-30") # beg of preseason
-# tibble(
-#   date = seq.Date(from = ymd("2018-09-28"), to = today(), by = "days")) %>% 
-#   filter(!is.instant(date))
+
+addresses(address_filtered) %>% group_by(Department) %>% count(Department, word) %>% 
+                                mutate(proportion = n / sum(n)) %>% select(-n) %>% 
+                                pivot_wider(names_from = "Department", values_from = "proportion") %>%
+                                pivot_longer(cols = c(`LEON`,`MASAYA`,`ESTELI`,`GRANADA`), names_to = "Department",values_to="proportion") %>% 
+                      ggplot(aes(x = proportion, y = `MANAGUA`, color = abs(`MANAGUA` - proportion))) +
+                      geom_abline(color = "gray40", lty = 2) +
+                      geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+                      geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+                      scale_x_log10(labels = percent_format()) +
+                      scale_y_log10(labels = percent_format()) +
+                      scale_color_gradient(limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+                      facet_wrap(~Department, ncol = 2) +
+                      theme(legend.position="none") +
+                      labs(y = "MANAGUA", x = NULL) +
+                      ggtitle("Proportion of address in each department v Managua(Capital)")
+
+
+
+
+
+  
+  
+
 
 
 
