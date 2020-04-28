7a383c32ef21c3144a078a4efcd5efacf5e0a53d
Jose Ayerdis <joseayerdis@gmail.com>
2020-04-28 16:00:10 -0600

    Add word cloud more department

--- direcciones/wordcloud.R
+++ direcciones/wordcloud.R
@@ -15,7 +15,7 @@ addresses %>% filter(Department %in% c('MANAGUA', 'LEON','ESTELI','CHINANDEGA'))
   top_n(40, wt = n) %>%
   mutate(angle = 90*sample(c(0, 1), n(), replace=TRUE, prob = c(80,20))) %>%
   ggplot(aes(label = word, size = proportion, angle = angle)) +
-  geom_text_wordcloud_area(area_corr_power =  1) +
+  geom_text_wordcloud_area(area_corr_power =  1, rm_outside = TRUE) +
   scale_size_area(max_size = 20) +
   facet_wrap(~Department) + theme_minimal()
 
