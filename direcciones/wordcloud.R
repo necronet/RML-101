2766c54ba07335e5f692870ee941328f968c24f4
Jose Ayerdis <joseayerdis@gmail.com>
2020-04-28 15:55:52 -0600

    Add word cloud more department

--- direcciones/wordcloud.R
+++ direcciones/wordcloud.R
@@ -7,12 +7,12 @@ stop_words_es <- stopwords::stopwords(language = "es")
 addresses <- preprocess_original_file() %>% get_address() 
 
 
-addresses %>% filter(Department %in% c('MANAGUA', 'LEON')) %>% count(Department, word) %>%  
+addresses %>% filter(Department %in% c('MANAGUA', 'LEON','ESTELI','CHINANDEGA')) %>% count(Department, word) %>%  
   filter(!word %in% stop_words_es) %>%
   group_by(Department) %>%
   mutate(proportion = n / sum(n)) %>% 
   arrange(-n) %>%
-  top_n(75, wt = n) %>%
+  top_n(40, wt = n) %>%
   mutate(angle = 90*sample(c(0, 1), n(), replace=TRUE, prob = c(80,20))) %>%
   ggplot(aes(label = word, size = proportion, angle = angle)) +
   geom_text_wordcloud_area(area_corr_power =  1) +
