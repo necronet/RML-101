b492e75e43859d4229353dd8939aa8596c24d09b
Jose Ayerdis <joseayerdis@gmail.com>
2020-06-04 11:32:22 -0600

    Work on address classiciation

--- direcciones/multinomial_model.R
+++ direcciones/multinomial_model.R
@@ -10,10 +10,21 @@ library(workflows)
 address_data <- preprocess_original_file()
 
 data <- address_data %>% transmute(Id = row_number(), Department = as.factor(Department), Address = paste(Address1,Address2))
-
-recipe <- data %>% recipe(Department ~ .) %>% step_tokenize(Address) %>% 
+  
+#%>% step_lemma(Address) 
+recipe <- data %>% recipe(Department ~ .) %>% 
+          step_tokenize(Address) %>% 
           update_role(Id, new_role = "ID") %>%
           step_stopwords(Address, language = "es") %>%
           step_tokenfilter(Address) %>% step_tf(Address) 
 
+model_spec <- svm_poly("classification") %>% set_engine("kernlab")
+
+text_classification_wf <- workflows::workflow() %>% add_recipe(recipe) %>% add_model(model_spec)
+
+parsnip::fit(text_classification_wf, data)
+
 juice(prep(recipe)) %>% View()
+
+
+
