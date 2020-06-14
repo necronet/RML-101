242fa38cf0baa5b417160163b45043b27c6eddfc
Jose Ayerdis <joseayerdis@gmail.com>
2020-06-14 12:03:59 -0600

    Add another MCMC hasting example

--- coupling_montecarlo/metropolis.R
+++ coupling_montecarlo/metropolis.R
@@ -20,7 +20,8 @@ ggplot(data.frame(x), aes(x)) + geom_histogram(bins = 70)
 
 target <- function(x){
   if(x<0){
-    return(0)}
+    return(0)
+  }
   else {
     return( exp(-x))
   }
@@ -31,12 +32,11 @@ library(animation)
 
 animation::saveGIF( {
   
-oopt <- ani.options(interval = 0.2, nmax = 10)
+#oopt <- ani.options(interval = 0.2, nmax = 10)
 
-x = rep(0,10000)
-x[1] = 3     #this is just a starting value, which I've set arbitrarily to 3
-plots = c()
-for(i in 2:30000){
+x = rep(0,50000)
+x[1] = 3
+for(i in 2:50000){
   currentx = x[i-1]
   proposedx = currentx + rnorm(1,mean=0,sd=1)
   A = target(proposedx)/target(currentx) 
@@ -46,18 +46,18 @@ for(i in 2:30000){
     x[i] = currentx        # otherwise "reject" move, and stay where we are
   }
   
-  if(i %% 1000 == 0) {
-    dev.hold()
-    hist(x, main = paste0("MCMC Metropolis hasting algorithm, iteration: ",i))
-    ani.pause()
-  }
+  # if(i %% 1000 == 0) {
+  #   dev.hold()
+  #   hist(x, main = paste0("MCMC Metropolis hasting algorithm, iteration: ",i))
+  #   ani.pause()
+  # }
 }
 }, img.name = "MCMC-metropolis-hasting", title = "MCMC Metropolis hasting algorithm", 
 description = "To approximate posterior with target as exponential distribution")
 
 
-
-
+hist(x)
+ggplot(data.frame(x = 1:length(x), y = x), aes(x, y)) + geom_line()
 
 
 
